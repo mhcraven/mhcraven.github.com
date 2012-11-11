e3f2c7ffb758d3cb440dbc0bf48101ed517a3a3f
Melody <melody_craven@yahoo.com>
2012-11-11 17:49:17 -0500

    Sunday1

---
+++ 9/index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+
+<html id="home" lang="en">
+
+	<head>
+		<meta charset="UTF-8">
+		<link href="styles.css" rel="Stylesheet" type="text/css" />
+	<title> Eldur Vatn Resort</title>
+	</head>
+
+	<body>
+		<header>
+			<div id='logo-container'><a href='index.html'>Eldur Vatn logo</a>
+			</div>
+			<div id='header-rt'>
+				<div id='languages'>
+					<ul>
+						<li><a href="#">English</a></li>
+						<li><a href="#">Icelandic</a></li>
+						<li><a href="#">Francais</a></li>
+					</ul>
+				</div>
+				<fieldset id='signin-container'>
+					<legend id="form-title">Your Account</legend>
+					<form action="/" id="signin">
+							<input id="signin-name" type="text" value="username" />						
+							<input id="signin-psswrd" type="text" value="password" />
+							<input id="signin-submit" type="submit" value="enter" />
+					</form>
+				</fieldset>
+				<div>
+					<a href="chat.html" class="chat-icon">chat live with the hotel concierge</a>
+				</div>
+			</div>	
+		</header>	
+		<nav>
+			<ul id="top-nav">
+				<li class='rt-border'><a href="relax.html">Relax and Revive</a></li>
+				<li class='rt-border'><a href="#">See and Do</a></li>
+				<li><a href="#">Stay</a></li>
+			</ul>	
+		</nav>
+		<section id='main-content'></section>
+		<footer>
+			<ul>
+				<li><a href="#">Getting Here</a></li>
+				<li><a href="#">Privacy Policy</a></li>
+				<li><a href="#">Contact Us</a></li>	
+			</ul>
+		</footer>
+	</body>
+</html>
